d1ce80091026e5740855aaaf9eb4b08a50622be2
João Rosa <joaoasrosa@gmail.com>
2019-02-21 14:10:38 +0100

    Add bootstrap code

--- main.go
+++ main.go
@@ -1,1 +1,14 @@
 package http_health_exporter
+
+import (
+	"net/http"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+func main() {
+	http.Handle("/metrics", promhttp.Handler())
+	log.Info("Beginning to serve on port :9545")
+	log.Fatal(http.ListenAndServe(":9545", nil))
+}
